567507f8ad70f3bac6a37d9e2666737527b9ffad
Jonathan Heathcote <mail@jhnet.co.uk>
2014-07-08 09:52:59 +0100

    Added simple setuptools script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='WaveDromTikZ',
+      version='0.1',
+      description='WaveDrom to TikZ converter.',
+      author='Jonathan Heathcote',
+      author_email='mail@jhnet.co.uk',
+      scripts=['wavedromtikz.py'],
+     )
