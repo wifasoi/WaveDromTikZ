6151876fe07757b693dd12215b93edcf2bf23c69
Luca Cristaldi <wifasoi@gmail.com>
2021-12-05 23:18:25 +0100

    Bump version
    Fix pyyaml dependency
    Add myself as maintainer in the setup.py file

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@
 from setuptools import setup
 
 setup(name='WaveDromTikZ',
-      version='0.1',
+      version='0.1.1',
       description='WaveDrom to TikZ converter.',
       author='Jonathan Heathcote',
       author_email='mail@jhnet.co.uk',
+      maintainer='Luca Cristaldi',
       scripts=['wavedromtikz.py'],
+      install_requires=['pyyaml']
       )
